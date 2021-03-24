2de309cf8d9bf3fbd6468ffeba7c6219828bcee5
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-03-24 16:42:58 -0300

    Exercicio 6

---
+++ exercises/bloco_4/dia_1/Exercicio-do-dia/exercicio6.js
@@ -0,0 +1,10 @@
+let nomePeca = "Cavalo";
+let nomePecaMinusculo = nomePeca.toLowerCase();
+
+console.log(nomePecaMinusculo)
+
+if(nomePecaMinusculo === "cavalo") {
+  console.log("Movimentos em L")
+} else {
+  console.log("Erro, essa peça não existe")
+}
