1390694126bae79363f38a2fd7335b66f23355cf
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-03-24 16:25:49 -0300

    3 Exercicio

---
+++ exercises/bloco_4/dia_1/Exercicio-do-dia/exercicio3.js
@@ -0,0 +1,11 @@
+let a = 3;
+let b = 4;
+let c = 5;
+
+if (a > b && a > c) {
+  console.log("A é o maior numero")
+} else if (b > a && a > c) {
+  console.log("B é o maior numero")
+} else {
+  console.log("C é o maior numero")
+}
