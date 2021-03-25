bed38efd6f95e226f9400e6b59444885740a6cff
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-03-25 14:41:44 -0300

    For e For Of, exercicios.

---
+++ exercises/bloco_4/dia_2/Conteúdos-Exercicios/for-of.js
@@ -0,0 +1,8 @@
+// Exercício 1
+// Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let elementos of names) {
+  console.log(elementos)
+}
