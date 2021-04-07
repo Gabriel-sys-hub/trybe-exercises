dc0d407a49f639a8fa5f5dddabb802707b1ece47
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-04-07 10:52:23 -0300

    Exercicio Pós Aula

--- exercises/bloco_5/dia_3/Exercicio Pós Aula/script.js
+++ exercises/bloco_5/dia_3/Exercicio Pós Aula/script.js
@@ -19,9 +19,10 @@ const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
 
 const uList = document.querySelector('#days');
 const buttonsContainer = document.querySelector('.buttons-container');
+let listOfLi = document.createElement('li');
 
 for(let index = 0; index < 33; index += 1) {
-  let listOfLi = document.createElement('li');
+  listOfLi = document.createElement('li');
   uList.appendChild(listOfLi)
   listOfLi.classList.add('day');
   listOfLi.innerHTML = dezDaysList[index]
@@ -42,7 +43,6 @@ function holiday() {
   buttom.setAttribute('id', 'btn-holiday')
   buttom.innerHTML = 'Feriados'
   buttonsContainer.appendChild(buttom)
-  changeColors()
 }
 
 function changeColors() {
@@ -52,9 +52,9 @@ function changeColors() {
 }
 
 
-buttom.addEventListener('click', holiday);
+buttom.addEventListener('click', changeColors);
+holiday();
 
-holiday()
 
 function friday() {
   buttomFriday.setAttribute('id', 'btn-friday');
@@ -62,4 +62,51 @@ function friday() {
   buttonsContainer.appendChild(buttomFriday);
 }
 
+
+function changeText() {
+  for(let index = 0; index < listOfEveryHoliday.length; index += 1) {
+    listOfEveryFriday[index].innerHTML = 'XABLAUUUU'
+   }
+    buttomFriday.addEventListener('change', changeText); 
+}
+
+buttomFriday.addEventListener('click', changeText )
 friday();
+
+let listOfLisArray = document.querySelectorAll('.day');
+
+function zoom() {
+  for(let index = 0; index < 33; index += 1) {
+    listOfLisArray[index].classList.add('zoom');
+   }
+}
+
+function activeTab(index) {
+  listOfLisArray.forEach((section) => {
+    section.classList.remove('zoom');
+  })
+  listOfLisArray[index].classList.add('zoom');
+}
+
+listOfLisArray.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('mouseover', () => {
+    activeTab(index);
+  })
+})
+
+let myTasks = document.querySelector('.my-tasks');
+let addButton = document.querySelector('#btn-add');
+
+function tasks() {
+  let span = document.createElement('span');
+  let inputValue = document.getElementById('task-input').value;
+  let text = document.createTextNode(inputValue);
+  span.appendChild(text);
+  if (inputValue === '') {
+    alert('You must write something!')
+  }
+  myTasks.appendChild(span)
+  document.getElementById('task-input').value = ''
+}
+
+addButton.addEventListener('click', tasks)
