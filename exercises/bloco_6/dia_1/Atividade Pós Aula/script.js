1dbe2e163da71092699a1790e5da9d0fd01c479b
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-04-16 14:07:58 -0300

    Exercises

--- exercises/bloco_6/dia_1/Atividade Pós Aula/script.js
+++ exercises/bloco_6/dia_1/Atividade Pós Aula/script.js
@@ -1,35 +1,6 @@
 const statesContainer = document.getElementById('estados');
-const initialJobDate = document.getElementById('data');
-const initialJobMonth = document.getElementById('mes');
-const initialJobYear = document.getElementById('ano');
-
-initialJobDate.addEventListener('change', () => {
-  if (initialJobDate.value > 0 && initialJobDate.value <= 31) {
-    initialJobDate.value;
-    console.log(initialJobDate.value);
-  } else {
-    alert('Data incorreta!');
-    initialJobDate.value = 1;
-  }
-});
-
-initialJobMonth.addEventListener('change', () => {
-  if (initialJobMonth.value > 0 && initialJobMonth.value <= 12) {
-    initialJobMonth.value;
-  } else {
-    alert('Mês incorreta!');
-    initialJobMonth.value = 1;
-  }
-});
-
-initialJobYear.addEventListener('change', () => {
-  if (initialJobYear.value > 0) {
-    initialJobMonth.value
-  } else {
-    alert('Ano incorreta!');
-    initialJobMonth.value = 1;
-  }
-});
+const container = document.getElementById('container-resume');
+const btnSubmit = document.getElementById('submit');
 
 function generateStates() {
   const states = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
@@ -40,17 +11,17 @@ function generateStates() {
   }
 }
 generateStates();
-const submiteBtn = document.getElementById('submit');
-const saveData = document.getElementsByTagName('form');
-console.log(saveData)
 
-submiteBtn.addEventListener('click', () => {
-  const savedData = localStorage.setItem('key', saveData.innerText);
+const form = document.getElementById('form');
+const newForm = new FormData(form);
+
+btnSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  for (let index of newForm.entries()) {
+    const newDivElement = document.createElement('div');
+    const newDiv = document.getElementById('div');
+    newDivElement.innerHTML = `${index[0]}: ${index[1]}`;
+    newDiv.appendChild(newDivElement);   
+  }
 });
-
-function divWithForm() {
-  const newDivSection = document.createElement('div');
-  const loadData = localStorage.getItem('key');
-  body.appendChild(newDivSection);
-  newDivSection.appendChild(loadData);
-}
