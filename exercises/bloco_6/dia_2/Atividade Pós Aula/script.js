9d15cf040b56ee951c966fd685493b16e6fd9bd8
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-04-19 17:22:50 -0300

    day3

--- exercises/bloco_6/dia_2/Atividade Pós Aula/script.js
+++ exercises/bloco_6/dia_2/Atividade Pós Aula/script.js
@@ -1,6 +1,7 @@
 const statesContainer = document.getElementById('estados');
 const container = document.getElementById('container-resume');
 const btnSubmit = document.getElementById('submit');
+const email = document.getElementById('email');
 
 function generateStates() {
   const states = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
@@ -12,16 +13,6 @@ function generateStates() {
 }
 generateStates();
 
-const form = document.getElementById('form');
-const newForm = new FormData(form);
-
-btnSubmit.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  for (let index of newForm.entries()) {
-    const newDivElement = document.createElement('div');
-    const newDiv = document.getElementById('div');
-    newDivElement.innerHTML = `${index[0]}: ${index[1]}`;
-    newDiv.appendChild(newDivElement);   
-  }
-});
+validation.init('#form');
+validation.isValid('#form')
+validation.show(email, 'message');
