e1d6d0a4f7b382d8f10968cc32aa8cbb27c518c0
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-05-24 16:02:47 -0300

    Front End Dia 1 Bloco 1

--- 2 FRONT_END/bloco_1/dia_1/hello-world/src/App.js
+++ 2 FRONT_END/bloco_1/dia_1/hello-world/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const arrayTasks = ['Wake up', 'Wash teth', 'Eat', 'Play a Game', 'Study', 'Lunch']
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 1500'},
+    { nome: 'Bola de Futebol', preco: 'R$ 20'},
+    { nome: 'Ferrari', preco: 'R$ 100'},
+    { nome: 'Mercedez', preco: 'R$ 1500'},
+    { nome: 'Notebook', preco: 'R$ 1500'},
+  ],
+  ativa: true,
+}
+
+const App = () => {
+  const total = mario.compras.map((eachItem) => Number(eachItem.preco.replace('R$ ', ''))).reduce((acc, number) => acc + number);
+  console.log(total)
+    return (
+      <>
+        <p>Nome: {mario.cliente}</p>
+        <p>Idade: {mario.idade}</p>
+        <p>Situação: <span style={{ color: mario.ativa ? 'green' : 'red' }}>{mario.ativa ? 'Ativo' : 'Inativo'}</span></p>
+        <p>Total: R$ {total}</p>
+        <p>{total > 10000 ? 'Você está gastando de mais patrão!' : 'Muito bem, está com as dividas organizadas'}</p>
+        <ul>{arrayTasks.map(tarefa => <li key={tarefa}>{tarefa}</li>) }</ul>
+      </>
   );
 }
+
+ReactDOM.render(App, document.getElementById('root'))
 
 export default App;
