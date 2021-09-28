67a8f7c75e9128e741351c7b88d9f60ca87ec213
gabri <gabriel@ubuntu.ubuntu-domain>
2021-09-28 12:50:24 -0300

    Dia 2

--- 3_BACK_END/26.2_Node.js/atividade4.js
+++ 3_BACK_END/26.2_Node.js/atividade4.js
@@ -11,14 +11,30 @@ const fs = require('fs').promises;
 //   strings.forEach((string) => console.log(string));
 // });
 
-async function createSimpsonsFamily() {
-  const simpsonFamily = await fs.readFile('./simpson.json', 'utf8');
-  const resultFromSimpson = await JSON.parse(simpsonFamily);
-  const result = await resultFromSimpson.map(({ id, name }) => `${id} - ${name}`);
-  const stringResult = result.forEach(string => console.log(string))
-  console.log(stringResult)
-  return stringResult;
+// async function createSimpsonsFamily() {
+// 	const simpsonFamily = await fs.readFile('./simpson.json', 'utf8');
+// 	const resultFromSimpson = await JSON.parse(simpsonFamily);
+// 	const result = await resultFromSimpson.map(({ id, name }) => `${id} - ${name}`);
+// 	const stringResult = result.forEach((string) => console.log(string));
+// 	console.log(stringResult);
+// 	return stringResult;
+// }
+
+async function getSimpsonById(id) {
+	const simpsons = await fs.readFile('./simpson.json', 'utf-8').then((fileContent) => JSON.parse(fileContent));
+
+	const chosenSimpson = simpsons.find((simpson) => simpson.id === id);
+
+  console.log(chosenSimpson);
+
+	// if (!chosenSimpson) {
+	// 	/* A palavra-chave `throw` dispara um erro que deve ser tratado por quem chamou nossa função.
+  //  * Em funções `async`, utilizar `throw` faz com que a Promise seja rejeitada,
+  //  * tendo como motivo o que passarmos para o `throw`.
+  //  * Ou seja, a linha abaixo rejeita a Promise da nossa função com o motivo 'id não encontrado'
+  //  */
+	// 	throw new Error('id não encontrado');
+	// }
 }
 
-
-createSimpsonsFamily();
+getSimpsonById(5);
